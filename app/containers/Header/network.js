4f784dd6c5e671bc225f5a732f96acb823e1467d
Haouhat <da_haouhat@esi.dz>
2019-08-21 16:33:34 +0100

    Updates

--- app/containers/Header/network.js
+++
@@ -1,9 +0,0 @@
-
-const Network = {
-  Offline: { rpc: 'offline', tx_explorer: null },
-  'Local RPC': { rpc: 'http://127.0.0.1:8545', tx_explorer: null },
-  'Ropsten Testnet': { rpc: 'https://ropsten.infura.io/efeead0c71c94eaf801c86c30eb90e69', tx_explorer: 'https://ropsten.etherscan.io/tx/' },
-  'Main Net': { rpc: 'https://mainnet.infura.io/efeead0c71c94eaf801c86c30eb90e69', tx_explorer: 'https://etherscan.io/tx/' },
-};
-
-module.exports = Network;
